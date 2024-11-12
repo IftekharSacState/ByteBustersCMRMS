dfa92675cacfe7fd95ee1c365e6ca9be428f9b47
Suyesh Shrestha <sshrestha3@csus.edu>
2024-11-12 14:22:02 -0800

    feedback class, but needs more work

--- Backend/Classes/feedbackClass.js
+++ Backend/Classes/feedbackClass.js
@@ -2,4 +2,15 @@
 
 class Feedback {
     
+    /**
+     * 
+     */
+    constructor(stars, content, mechanicId, customerId, date) {
+        this.stars = stars;
+        this.content = content;
+        this.mechanicId = mechanicId;
+        this.customerId = customerId;
+        this.date = new Date();
+        this.date = date;
+    }
 }
