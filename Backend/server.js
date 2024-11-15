3620b7f256f084c13832fb4d5c3a464e2342b2c2
Iftekhar Ahmad <iahmad@csus.edu>
2024-11-14 16:01:12 -0800

    folder name change

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const db = require("./db");
+const db = require("./db.js");
 
 // Load environment variables
 dotenv.config();
