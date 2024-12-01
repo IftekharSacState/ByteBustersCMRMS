6d09f8212d2d0712eacdebdd9f67e35fcd00744e
Wilson <wluong@csus.edu>
2024-12-01 03:38:45 -0800

    Nav Register New Acc, Login

--- Frontend/login/index.js
+++ Frontend/login/index.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const contactButton = document.querySelector(".contact");
     const servicesButton = document.querySelector(".services-plus");
     const aboutButton = document.querySelector(".about");
-  
+    const loginButton = document.querySelector(".login-button"); 
+    const registerButton = document.querySelector(".button"); 
+
     // signInButton.addEventListener("click", () => {
     //   window.location.href = "../login/index.html";
     //   // Add your sign-in logic here
@@ -34,5 +36,13 @@ document.addEventListener("DOMContentLoaded", () => {
     aboutButton.addEventListener("click", () => {
       window.location.href = "../about/index.html";
       // Add your about logic here
+    });
+    loginButton.addEventListener("click", () => {
+      // Redirect to custdash.html
+      window.location.href = "../custdash/index.html";
+    });
+    registerButton.addEventListener("click", () => {
+      // Redirect to register.html
+      window.location.href = "../signup/index.html";
     });
   });
